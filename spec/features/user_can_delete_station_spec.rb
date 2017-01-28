e7ec230f700cb2f7015922a654c4e6076e81a762
blackknight75 <devbox4714@gmail.com>
2017-01-28 14:21:40 -0700

    added delete and edit tests for stations/index. Refactored tests.

--- spec/features/user_can_delete_station_spec.rb
+++ spec/features/user_can_delete_station_spec.rb
@@ -13,5 +13,16 @@ describe 'user deletes station' do
     expect(Station.all.count).to eq(0)
   end
 
+  it 'can be deleted from index page' do
+    Station.create(name: Faker::Cat.name, dock_count: Faker::Number.number(1),
+    city: Faker::Space.galaxy, installation_date: '01/01/2017')
+
+    visit('/stations')
+    click_on('Delete')
+
+    expect(page).to have_current_path('/stations')
+    expect(Station.all.count).to eq(0)
+  end
+
   # needs delete test for index page
 end
--- spec/features/user_sees_all_stations_and_attributes_spec.rb
+++ spec/features/user_sees_all_stations_and_attributes_spec.rb
@@ -1,12 +1,35 @@
 require_relative '../spec_helper'
+require 'pry'
 
-describe "When user visits index" do
-  it "they see a station" do
-    visit('/stations')
+def station_params
+  {
+    name: Faker::Cat.name,
+    dock_count: Faker::Number.number(1),
+    city: Faker::Space.galaxy,
+    installation_date: '01/02/2017'
+  }
+end
 
-    save_and_open_page
-    within('#station_greeting') do
-      expect(page).to have_content("Stations")
+describe "When user visits stations/index" do
+  it "they see all stations" do
+    5.times { Station.create(station_params) }
+
+    Station.all.each do |station|
+      visit('/stations')
+
+      expect(page).to have_content(station.name)
+      expect(page).to have_content(station.dock_count)
+      expect(page).to have_content(station.city)
+      expect(page).to have_content(station.installation_date)
     end
   end
+
+  it 'can redirect to edit from stations/index' do
+    Station.create(station_params)
+    visit('/stations')
+    click_on('Edit')
+
+    expect(page).to have_current_path('/stations/1/edit')
+  end
+
 end
