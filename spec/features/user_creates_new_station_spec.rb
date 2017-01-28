0cb11827d14d516b71ed48a8e5e92d9a31a667b3
Katy Farmer <dreadpiratekaty@gmail.com>
2017-01-28 12:12:44 -0700

    Adds new test and erb

---
+++ spec/features/user_creates_new_station_spec.rb
@@ -0,0 +1,15 @@
+require_relative "../spec_helper"
+
+describe "when a user visits the new form page" do
+  it "sees the form to create a new station" do
+    visit "/stations/new"
+    fill_in("station[name]", with: "SF")
+    fill_in("station[dock_count]", with: "59")
+    fill_in("station[city]", with: "Guadalajara")
+    fill_in("station[installation_date]", with: "01/01/2017")
+    click_on("Submit")
+
+    expect(current_path).to eq("/stations/1")
+    expect(page).to have_content("SF")
+  end
+end
