a8dc4e4468245dcceb719a843305d34fedbc871b
blackknight75 <devbox4714@gmail.com>
2017-01-27 18:28:58 -0700

    Adds routes and models

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,2 +1,6 @@
 class Station < ActiveRecord::Base
+  validates :name, presence: true
+  validates :dock_count, presence: true
+  validates :city, presence: true
+  validates :installation_date, presence: true
 end
--- spec/models/station_spec.rb
+++ spec/models/station_spec.rb
@@ -1,1 +1,29 @@
 require_relative '../spec_helper'
+
+describe Station do
+  describe "validations" do
+    it "invalid without a name" do
+      station = Station.create(dock_count: 22, city: "Denver", installation_date: "01/01/2017")
+
+      expect(station).to_not be_valid
+    end
+
+    it "invalid without a dock count" do
+      station = Station.create(name: "Big Deal", city: "Denver", installation_date: "01/01/2017")
+
+      expect(station).to_not be_valid
+    end
+
+    it "invalid without a city" do
+      station = Station.create( name: "Art Center", dock_count: 22, installation_date: "01/01/2017")
+
+      expect(station).to_not be_valid
+    end
+
+    it "invalid without a installation date" do
+      station = Station.create(name: "Corner Store", dock_count: 22, city: "Denver")
+
+      expect(station).to_not be_valid
+    end
+  end
+end
