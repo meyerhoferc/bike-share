98f6430abe7223304a3f81f0c20e4949810f662f
Courtney Meyerhofer <meyerhoferc@gmail.com>
2017-01-29 11:25:51 -0700

    adds routes and form for new station

--- app/controllers/bike_share_app.rb
+++ app/controllers/bike_share_app.rb
@@ -66,6 +66,35 @@ class BikeShareApp < Sinatra::Base
    erb :"trip/index"
  end
 
+ get '/trips/new' do
+   erb :"trip/new"
+ end
+
+ post '/trips' do
+   bike = Bike.create(bike_number: params[:trip][:bike])
+   subscription = Subscription.create(account: params[:trip][:subscription])
+   zipcode = Zipcode.create(zip_code: params[:trip][:zipcode])
+   start_station = Station.find_by(name: params[:trip][:start_station])
+   end_station = Station.find_by(name: params[:trip][:end_station])
+  #  require 'pry'; binding.pry
+   start_date = params[:trip][:start_date]
+   end_date = params[:trip][:end_date]
+   duration = params[:trip][:duration]
+   input = {
+     bike_id: bike.id,
+     subscription_id: subscription.id,
+     zipcode_id: zipcode.id,
+     start_station_id: start_station.id,
+     end_station_id: end_station.id,
+     start_date: start_date,
+     end_date: end_date,
+     duration: duration
+   }
+   trip = Trip.create(input)
+
+   redirect "/trips/#{trip.id}"
+ end
+
  get '/trips/:id' do
    @trip = Trip.find(params[:id])
    erb :"trip/show"
