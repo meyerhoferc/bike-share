392e6e7ea74c9ef4c7a0e2f85e3535e5e5d2972d
Courtney Meyerhofer <meyerhoferc@gmail.com>
2017-01-31 11:22:48 -0700

    adds shortest_ride method to trip model

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -6,12 +6,14 @@ class Trip < ActiveRecord::Base
   validates :end_station, presence: true
   validates :bike, presence: true
   validates :subscription, presence: true
-  # validates :zipcode, presence: true
-  # validates_uniqueness_of
 
   belongs_to :start_station, class_name: 'Station', foreign_key: :start_station_id
   belongs_to :end_station, class_name: 'Station', foreign_key: :end_station_id
   belongs_to :bike
   belongs_to :subscription
   belongs_to :zipcode
+
+  def self.shortest_ride
+    minimum(:duration)
+  end
 end
