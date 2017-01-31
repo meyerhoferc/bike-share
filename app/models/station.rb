f1b08d2e68f99271452760ee15ed2dae57d5c8e2
Katy Farmer <dreadpiratekaty@gmail.com>
2017-01-30 18:20:37 -0700

    Adds station dashboard

--- app/models/station.rb
+++ app/models/station.rb
@@ -8,4 +8,36 @@ class Station < ActiveRecord::Base
   def self.average_bikes
     average(:dock_count).round(1)
   end
+
+  def self.fewest_bikes
+    minimum(:dock_count)
+  end
+
+  def self.name_fewest_bikes
+    self.where(dock_count: minimum(:dock_count)).map do |station|
+      station.name
+    end.join
+  end
+
+  def self.most_bikes
+    maximum(:dock_count)
+  end
+
+  def self.name_most_bikes
+    self.where(dock_count: maximum(:dock_count)).map do |station|
+      station.name
+    end.join
+  end
+
+  def self.newest_station
+    max_date = maximum(:installation_date)
+    station_name = self.find_by(installation_date: maximum(:installation_date)).name
+    "#{station_name}: #{max_date.month}/#{max_date.day}/#{max_date.year}"
+  end
+
+  def self.oldest_station
+    min_date = minimum(:installation_date)
+    station_name = self.find_by(installation_date: minimum(:installation_date)).name
+    "#{station_name}: #{min_date.month}/#{min_date.day}/#{min_date.year}"
+  end
 end
